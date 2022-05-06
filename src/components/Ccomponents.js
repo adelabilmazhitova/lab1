5228486ee4e03627f60460cd15d01ea7c8ce32ad
adelabilmazhitova <102782937+adelabilmazhitova@users.noreply.github.com>
2022-05-06 14:02:45 +0300

    first commit

---
+++ src/components/Ccomponents.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class 
+ extends Component {
+    
+  render() {
+    return (
+      <div>
+          <h1>This project {this.props.num}</h1>
+      </div>
+    )
+  }
+}
---
+++ src/components/Fcomponents.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Fcomponents(props) {
+  return (
+    <div>
+        <h1>Welcome, {props.name}</h1>
+    </div>
+  )
+}
